4a3bc6b6c65cabe9be1bc143027c05022958f380
Taylor Munroe <t.munroe@aol.co.uk>
2018-12-18 03:10:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,3 +23,18 @@ function inc(a){
   number = a + 1;
   return number;
 }
+
+function dec(a){
+  number = a - 1;
+  return number;
+}
+
+function makeInt(a){
+  number = parseInt(a, 10);
+  return number;
+}
+
+function preserveDecimal(a){ 
+  number = parseFloat(a, 10);
+  return number;
+}
